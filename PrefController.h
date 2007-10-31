bb604f58d17a4d35278b523e8840c04303147e9d
mattswell <mattswell@a754d6da-1b3d-0410-9972-756c9b655a87>
2007-10-31 22:33:46 +0000

    pimped out preferences

--- PrefController.h
+++ PrefController.h
@@ -1,10 +1,11 @@
 #import <Cocoa/Cocoa.h>
 #import "DBPrefsWindowController.h"
 
-@interface PrefController : DBPrefsWindowController
-{
-  IBOutlet NSView *generalPrefsView;
-  IBOutlet NSView *soundPrefsView;
+@interface PrefController : DBPrefsWindowController {
+    IBOutlet NSView *generalPrefsView;
+    IBOutlet NSView *soundPrefsView;
+    IBOutlet NSButton *previewButton;
+	BOOL previewState;
 }
 
 @end
--- PrefController.m
+++ PrefController.m
@@ -2,10 +2,42 @@
 
 @implementation PrefController
 
+- (id)initWithWindow:(NSWindow *)window
+  // -initWithWindow: is the designated initializer for NSWindowController.
+{
+	self = [super initWithWindow:nil];
+	if (self != nil) {
+		previewState = NO;
+	}
+	return self;
+
+	(void)window;  // To prevent compiler warnings.
+}
+
 - (void)setupToolbar
 {
   [self addView:generalPrefsView label:@"General"];
   [self addView:soundPrefsView label:@"Sound"];
+}
+
+- (IBAction)preview_clicked:(id)sender
+{
+	if (!previewState)
+	{
+			// play sound
+		//cmd!
+			// change button text to "Stop"
+		[previewButton setTitle:@"Stop"];
+		previewState = !previewState;
+	}
+	else
+	{
+			// stop sound
+		//cmd!
+			// change button text to "Preview"
+		[previewButton setTitle:@"Preview"];
+		previewState = !previewState;
+	}
 }
 
 @end
